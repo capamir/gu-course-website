87937e952d054df41450ccd276a78e5b735374d6
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-11 11:20:40 +0330

    Order, OrderItem and Customer admin page

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Collection
+from .models import Product, Collection, Customer, Order, OrderItem
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
@@ -40,5 +40,51 @@ class CollectionAdmin(admin.ModelAdmin):
         )
 
 
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name',  'membership', 'orders']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+
+
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = OrderItem
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+    list_display = ['id', 'placed_at', 'customer']
+    raw_id_fields= ['customer',]
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'product']
+    raw_id_fields= ['order', 'product']
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderAdmin)
