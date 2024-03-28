aa367d8e5990ee8b3c63a42b348eb0f98b39c1da
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-29 01:43:41 +0330

    Bucket design finished

--- frontend/src/pages/Bucket.tsx
+++ frontend/src/pages/Bucket.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { IoCartOutline, IoWalletSharp } from "react-icons/io5";
+import { Toman } from "../components";
 
 const Bucket = () => {
-  return <div>Bucket</div>;
+  return (
+    <Card marginY={6}>
+      <CardHeader>
+        <Flex>
+          <HStack>
+            <IoCartOutline size={42} color="#0693e3" />
+            <Heading as="h2" fontFamily="fontBold" fontSize="2xl">
+              سفارشات
+            </Heading>
+          </HStack>
+          <Spacer />
+          <Button
+            leftIcon={<IoWalletSharp size={24} />}
+            colorScheme="green"
+            variant="solid"
+            borderRadius="3xl"
+            size="md"
+          >
+            ثبت نهایی
+          </Button>
+        </Flex>
+      </CardHeader>
+      <CardBody>
+        <SimpleGrid spacing={6}>
+          <Card borderRadius="xl" bg="gray.50">
+            <CardBody>
+              <Flex gap={5} alignItems="center">
+                <Heading as="h4" fontSize="lg" fontFamily="fontBold">
+                  آموزش داکر
+                </Heading>
+                <Text>مدرس: صادق اسکندری</Text>
+                <Spacer />
+                <HStack marginX="auto" marginBottom={{ base: 4, md: "0" }}>
+                  <Text
+                    color="brand-primary"
+                    fontFamily="fontBold"
+                    fontSize="lg"
+                  >
+                    3,500,000
+                  </Text>
+                  <Toman />
+                </HStack>
+                <Button>حذف</Button>
+              </Flex>
+            </CardBody>
+          </Card>
+        </SimpleGrid>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default Bucket;
