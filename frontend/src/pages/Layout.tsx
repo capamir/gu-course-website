4a3d1ed5865c141225318cf100bcbd4f64b8017b
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-28 17:02:51 +0330

    color mode

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,12 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { Footer, Navbar } from "../components";
 
 const Layout = () => {
+  const { colorMode } = useColorMode();
   return (
     <>
       <Navbar />
-      <Box padding={5}>
+      <Box padding={5} bg={colorMode === "dark" ? "" : "gray.50"}>
         <Outlet />
       </Box>
       <Footer />
