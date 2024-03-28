a09bf0ef36cb1d3a8d7361057fae36eb280553ed
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-28 04:16:39 +0330

    build currency common components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -4,3 +4,5 @@ export { default as CourseHeading } from "./Courses/CourseHeading";
 export { default as CourseGrid } from "./Courses/CourseGrid";
 export { default as CourseCardContainer } from "./Courses/CourseCardContainer";
 export { default as CourseCard } from "./Courses/CourseCard";
+
+export { default as Toman } from "./commpn/Toman";
