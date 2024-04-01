3648d635d965ee132d0bd36f5005e440c8c9b69e
amirhossein <79211332+capamir@users.noreply.github.com>
2024-04-01 19:25:22 +0330

    update backend url

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://localhost:8000/",
 });
 
 export class APIClient<T> {
@@ -25,7 +25,7 @@ export class APIClient<T> {
 }
 
 const authInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://localhost:8000/",
 });
 
 export class AuthAPIClient<T> {
