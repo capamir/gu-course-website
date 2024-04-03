c09c1de08e0d85adc2fcc5724cebda195b050c79
amirhossein <79211332+capamir@users.noreply.github.com>
2024-04-03 11:10:19 +0330

    fetching product data hooks

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useAuth } from "./useAuth";
 export { default as useProducts } from "./useProducts";
+export { default as useProduct } from "./useProduct";
--- frontend/src/hooks/useProduct.tsx
+++ frontend/src/hooks/useProduct.tsx
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { ProductType } from "../types/Product";
 import { ProductApiClient } from "../services/apiServices";
 
-const useProducts = () =>
-  useQuery<ProductType[], Error>({
-    queryKey: ["products"],
-    queryFn: () => ProductApiClient.getAll(),
+const useProduct = (id: string) =>
+  useQuery<ProductType, Error>({
+    queryKey: ["products", id],
+    queryFn: () => ProductApiClient.get(id),
     staleTime: 60 * 60 * 1000,
   });
 
-export default useProducts;
+export default useProduct;
