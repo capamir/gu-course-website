c04a67951461e087dba60b732ddbb95db6229c75
amirhossein <79211332+capamir@users.noreply.github.com>
2024-04-03 11:09:39 +0330

    dynamic description with read more

--- frontend/src/components/Details/Description.tsx
+++ frontend/src/components/Details/Description.tsx
@@ -1,7 +1,24 @@
-import { Card, CardBody, Heading, HStack, Text } from "@chakra-ui/react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import {
+  Button,
+  Card,
+  CardBody,
+  Collapse,
+  Heading,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
 import { GiNotebook } from "react-icons/gi";
 
-const description = () => {
+interface Props {
+  description: string;
+}
+
+const description: React.FC<Props> = ({ description }) => {
+  const [show, setShow] = React.useState(false);
+
+  const handleToggle = () => setShow(!show);
   return (
     <Card marginY={6}>
       <CardBody>
@@ -11,22 +28,21 @@ const description = () => {
             توضیحات
           </Heading>
         </HStack>
-        <Text>
-          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
-          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
-          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
-          در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
-          طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
-          الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
-          صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
-          شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
-          اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
-          قرار گیرد. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ،
-          و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-          در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
-          و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-        </Text>
+        <Collapse startingHeight={150} in={show}>
+          <Text>{description}</Text>
+        </Collapse>
+        <Button
+          onClick={handleToggle}
+          mt="1rem"
+          colorScheme="green"
+          variant="solid"
+          borderRadius="3xl"
+          size="md"
+          fontFamily="fontBold"
+          fontSize="sm"
+        >
+          نمایش {show ? "کمتر" : "بیشتر"}
+        </Button>
       </CardBody>
     </Card>
   );
