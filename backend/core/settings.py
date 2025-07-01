c5347a06d45886d3778af266778e7f7ff712130b
amirhossein <79211332+capamir@users.noreply.github.com>
2025-07-02 01:25:44 +0330

    swagger added

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -132,7 +133,8 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ), 
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 admin.site.site_header = 'Storefront Admin'
 
@@ -11,6 +12,8 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('auth/', include('users.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 if settings.DEBUG:
